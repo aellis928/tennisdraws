52c4867495f620331c637011f53961ea5b8df430
aellis928 <mrskawasaki.90@gmail.com>
2016-05-31 14:40:11 -0700

    change api url

--- client/controllers/app.config.js
+++ client/controllers/app.config.js
@@ -1,4 +1,4 @@
-myApp.constant('API_URL', 'http://localhost:8000');
+myApp.constant('API_URL', 'http://54.186.88.86/');
 
 myApp.run(function($rootScope, $location, $route, AuthTokenFactory){
     // we use the AuthTokenFactory to bounce users from routes they
